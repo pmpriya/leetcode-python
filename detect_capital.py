936e59b97a0e79d948d3f941267c8b9bc2589f39
priyaapm <47645897+k1895306@users.noreply.github.com>
2022-01-24 08:00:19 +0000

    Add files via upload
    leetcode - 520.

---
+++ detect_capital.py
@@ -0,0 +1,22 @@
+class detect_capital:
+    def detectCapitalUse(self, word):
+        count = 0
+        flag = False
+        if(word[0].isupper()):
+            flag = True
+        for letter in word[1:]:
+            if(letter.isupper()):
+                count = count + 1
+        if count >= 1:
+            flag = False
+        if word.isupper() or flag:
+            return True
+        elif word.islower():
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    output = detect_capital("flaG")
+    print(output)
