fc534c890b56893747056c036d6765236a29a2a7
priyaapm <47645897+k1895306@users.noreply.github.com>
2022-01-27 12:00:00 +0000

    Update kadane_algorithm.py

--- kadane_algorithm.py
+++ kadane_algorithm.py
@@ -3,17 +3,11 @@
 
 class kadane_algorithm:
     def maxSubArray(self, nums):
-        max_till_now = nums[0]
-        max_ending = 0
-
-        for i in range(0, len(nums)):
-            max_ending = max_ending + nums[i]
-            if max_ending < 0:
-                max_ending = 0
-
-            elif (max_till_now < max_ending):
-                max_till_now = max_ending
-        return max_till_now
+        cur_max, max_till_now = 0, -inf
+        for c in nums:
+            cur_max = max(c, cur_max + c)
+            max_till_now = max(max_till_now, cur_max)
+        return max_till_now    
 
 if __name__ == '__main__':
     res = kadane_algorithm().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
